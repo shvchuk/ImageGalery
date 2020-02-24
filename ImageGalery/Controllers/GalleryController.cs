84d8799ee74bb0aeda0df5fdceff834724645166
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2020-02-24 16:08:41 +0100

    mock image gallery data

--- ImageGalery/Controllers/GalleryController.cs
+++ ImageGalery/Controllers/GalleryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ImageGalery.Models;
+using ImageGallery.data.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ImageGalery.Controllers
@@ -11,9 +12,59 @@ namespace ImageGalery.Controllers
     {
         public IActionResult Index()
         {
+            var mikoImageTags = new List<ImageTag>();
+            var rockyImageTags = new List<ImageTag>();
+
+            var tag1 = new ImageTag()
+            {
+                Description = "Adventure",
+                Id = 0
+            };
+
+            var tag2 = new ImageTag()
+            {
+                Description = "Dog",
+                Id = 1
+            };
+
+            var tag3 = new ImageTag()
+            {
+                Description = "Lesnica",
+                Id = 2
+            };
+
+            mikoImageTags.AddRange(new List<ImageTag>{ tag1, tag3 });
+            rockyImageTags.Add(tag2);
+
+            var imageList = new List<GalleryImage>() 
+            {
+                new GalleryImage()
+                {
+                    Title = "Miko",
+                    Url = "",
+                    Created = DateTime.Now,
+                    Tags = mikoImageTags
+                },
+
+                new GalleryImage()
+                {
+                    Title = "Rocky",
+                    Url = "",
+                    Created = DateTime.Now,
+                    Tags = rockyImageTags
+                },
+
+                new GalleryImage()
+                {
+                    Title = "Miko Park Lesnica",
+                    Url = "",
+                    Created = DateTime.Now,
+                    Tags = mikoImageTags
+                }
+            };
             var model = new GalleryIndexModel() 
             { 
-            
+                Images = imageList
             };
 
             return View(model);
